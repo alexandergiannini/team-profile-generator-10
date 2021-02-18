b637f693f85363a528661545ec66e0f8bcfe6607
Alexander Giannini <alexgiannini@alexanders-mbp.home>
2021-02-17 16:23:54 -0800

    added beginning functionality to append data to html page

---
+++ index.js
@@ -0,0 +1,119 @@
+const fs = require('fs')
+const inquirer = require('inquirer')
+
+const writeToHTML = (managerName, managerId, managerEmail, managerOfficeNumber) => {
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Profile Generator</title>
+</head>
+<body>
+    <header class="my-team-banner">
+        <h1>
+            My Team
+        </h1>
+    </header>
+    <section class="my-team-body">
+        <h2 class="manager-name">
+        ${managerName}
+        </h2>
+        <p class="manager-id">${managerId}</p>
+        <p class="manager-email">${managerEmail}</p>
+        <p class="manager-office-number">${managerOfficeNumber}</p>
+    </section>
+    
+</body>
+</html>`
+}
+
+const promptUser = data => {
+    return inquirer.prompt([
+        {
+        type: 'input',
+        name: 'managerName',
+        message: 'What is the name of your manager?',
+        validate: managerName => {
+            if (managerName && managerName.trim().length > 0) {
+                return true;
+            } else {
+                console.log('Please input the name of your manager.');
+                return false;
+            }
+        }
+        }, 
+        {
+        type: 'input',
+        name: 'managerId',
+        message: 'What is your managers ID?',
+        validate:  managerId => {
+            if (managerId && managerId.trim().length > 0) {
+                return true;
+            } else {
+                console.log('Please input the ID of your manager.');
+                return false;
+            }
+        }
+        },
+        {
+        type: 'input',
+        name: 'managerEmail',
+        message: 'What is your managers email?',
+        validate: managerEmail => {
+            if (managerEmail && managerEmail.trim().length > 0) {
+                return true;
+            } else {
+                console.log('You need to input a valid email for your manager.');
+                return false;
+            }
+        }
+        },
+        {
+        type: 'input',
+        name: 'managerOfficeNumber',
+        message: 'What is your managers office number?',
+        validate: managerOfficeNumber => {
+            if (managerOfficeNumber && managerOfficeNumber.trim().length > 0) {
+                return true;
+            } else {
+                console.log('You need to input your managers office number.');
+                return false;
+            }
+        }
+        },
+        {
+        type: 'list',
+        name: 'menuOption',
+        message: 'Would you like to add an engineer, an intern, or are you finished building your team?',
+        choices: ['Add an Engineer', 'Add an Intern', 'Finished Building'],
+        validate: menuOption => {
+            if (menuOption === 'Add an Engineer') {
+                return true;
+            } else {
+                console.log('You need to provide additional details about contributing guidelines for your project!');
+                return false;
+            }
+        }
+        },
+
+    ])
+} ////After questions are answered, the readme is generated below 
+//console.log(answers))
+promptUser().then(answers => fs.writeFile('./dist/index.html', writeToHTML(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber), err => {
+    if (err) {
+        throw err;
+    }
+})
+
+)
+  //.then(answers => console.log(answers));
+
+
+
+
+
+
+//console.log(inquirer)
