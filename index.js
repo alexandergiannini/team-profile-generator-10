248c7ff5d9afe758f68d937f3ef045cebe4b16a9
Alexander Giannini <alexgiannini@Alexanders-MacBook-Pro.local>
2021-02-18 23:26:33 -0800

    add styling

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const Manager = require('./lib/Manager')
 const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
 
+
+
 const writeToHTML = (managerName, managerId, managerEmail, managerOfficeNumber) => {
     return `
     <!DOCTYPE html>
@@ -13,6 +15,7 @@ const writeToHTML = (managerName, managerId, managerEmail, managerOfficeNumber) 
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"> 
     <link rel="stylesheet" href="style.css">
     <title>Team Profile Generator</title>
 </head>
@@ -23,17 +26,36 @@ const writeToHTML = (managerName, managerId, managerEmail, managerOfficeNumber) 
         </h1>
     </header>
     <section id="manager-section" class="employee-section">
-        <h2 class="manager-name">
-        Manager: ${managerName}
-        </h2>
-        <p class="manager-id">ID: ${managerId}</p>
-        <p class="manager-email">Email: ${managerEmail}</p>
-        <p class="manager-office-number">Office Number: ${managerOfficeNumber}</p>
+        <div class="container" id="manager-container">
+            <div class="row" id="manager-row">
+
+                <div class="col">
+
+                <h2 class="manager-name">
+                Manager: ${managerName}
+                </h2>
+                <p class="manager-id">ID: ${managerId}</p>
+                <a class="manager-email" href="mailto:${managerEmail}">Email: ${managerEmail}</a>
+                <p class="manager-office-number">Office Number: ${managerOfficeNumber}</p>
+
+
+
+
+                </div>
+
+            </div>
+
+        </div>
+        
     </section>
     
 </body>
 </html>`
 }
+
+//let myEng = new Engineer('Alexander', 2, 'alexgiannini310@gmail.com', 24, 'alexandergiannini')
+//console.log(myEng)
+//myEng.getName()
 
 const promptUser = data => {
     return inquirer.prompt([
@@ -90,22 +112,34 @@ const promptUser = data => {
         }
         },
         {
-        type: 'list',
+        type: 'input', ///'list'
         name: 'menuOption',
         message: 'Would you like to add an engineer, an intern, or are you finished building your team?',
         choices: ['Add an Engineer', 'Add an Intern', 'Finished Building'],
-        validate: menuOption => {
-            if (menuOption === 'Add an Engineer') {
-                return true;
+        validate: myMenuOption => {
+
+
+            if (myMenuOption === "Add an Engineer" || myMenuOption === 'add an engineer') {
+                return true
+               
+                
+            } else if (myMenuOption === "Add an Intern" || myMenuOption === "add an intern") {
+                return true
+            } else if (myMenuOption === 'Finished Building' || myMenuOption === 'finished building') {
+                return true
             } else {
-                console.log('You need to provide additional details about contributing guidelines for your project!');
-                return false;
+                console.log('Please input an appropriate answer.')
+                return false
             }
+            
         }
+         
+
+
         },
 
     ])
-} ////After questions are answered, the readme is generated below 
+} 
 //console.log(answers))
 promptUser().then(answers => fs.writeFile('./dist/index.html', writeToHTML(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber), err => {
     if (err) {
