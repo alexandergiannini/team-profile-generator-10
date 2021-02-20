4d18c49692711c1b14588258cb7fc661a9270b1c
Alexander Giannini <alexgiannini@Alexanders-MacBook-Pro.local>
2021-02-19 17:31:21 -0800

    added switch statements and increased validation based on user input

--- index.js
+++ index.js
@@ -4,10 +4,80 @@ const Employee = require('./lib/Employee')
 const Manager = require('./lib/Manager')
 const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
+const employeeList = []
+
+///create function that loops thru employeelist, where we check the role of each employee 3 if statements for manager intern engineer; have a variable that holds the string/html that will be generate as we go through the loop
+//
+
+const renderEmployees = () => {
+    let myEmployeeList = ''
+
+    for (let i = 0; i < employeeList.length; i++ ) {
+        if (employeeList[i].getRole() === 'Engineer') {
+
+
+            myEmployeeList += 
+
+            `<section id="manager-section" class="employee-section">
+        <div class="container" id="manager-container">
+            <div class="row" id="manager-row">
+
+                <div class="col">
+
+                <h2 class="manager-name">
+                Engineer: ${employeeList[i].getName()}
+                </h2>
+                <p class="manager-id">ID: ${employeeList[i].getId()}</p>
+                <a class="manager-email" href="mailto:${employeeList[i].getEmail()}">Email: ${employeeList[i].getEmail()}</a>
+                <p class="manager-office-number">Office Number: ${employeeList[i].getGithub()}</p>
 
 
 
-const writeToHTML = (managerName, managerId, managerEmail, managerOfficeNumber) => {
+                </div>
+
+            </div>
+
+        </div>
+        
+    </section>`
+
+
+
+        } else if (employeeList[i].getRole() === 'Intern') {
+
+        } else if (employeeList[i].getRole() === 'Manager') {
+            myEmployeeList += 
+
+            `<section id="manager-section" class="employee-section">
+        <div class="container" id="manager-container">
+            <div class="row" id="manager-row">
+
+                <div class="col">
+
+                <h2 class="manager-name">
+                Manager: ${employeeList[i].getName()}
+                </h2>
+                <p class="manager-id">ID: ${employeeList[i].getId()}</p>
+                <a class="manager-email" href="mailto:${employeeList[i].getEmail()}">Email: ${employeeList[i].getEmail()}</a>
+                <p class="manager-office-number">Office Number: ${employeeList[i].getOfficeNumber()}</p>
+
+
+
+
+                </div>
+
+            </div>
+
+        </div>
+        
+    </section>`
+        }
+    }
+    return myEmployeeList
+}
+
+
+const writeToHTML = () => {
     return `
     <!DOCTYPE html>
 <html lang="en">
@@ -25,29 +95,12 @@ const writeToHTML = (managerName, managerId, managerEmail, managerOfficeNumber) 
             My Team
         </h1>
     </header>
-    <section id="manager-section" class="employee-section">
-        <div class="container" id="manager-container">
-            <div class="row" id="manager-row">
 
-                <div class="col">
-
-                <h2 class="manager-name">
-                Manager: ${managerName}
-                </h2>
-                <p class="manager-id">ID: ${managerId}</p>
-                <a class="manager-email" href="mailto:${managerEmail}">Email: ${managerEmail}</a>
-                <p class="manager-office-number">Office Number: ${managerOfficeNumber}</p>
+    ${renderEmployees()}
 
 
 
-
-                </div>
-
-            </div>
-
-        </div>
-        
-    </section>
+    
     
 </body>
 </html>`
@@ -57,7 +110,7 @@ const writeToHTML = (managerName, managerId, managerEmail, managerOfficeNumber) 
 //console.log(myEng)
 //myEng.getName()
 
-const promptUser = data => {
+const promptManager = () => {
     return inquirer.prompt([
         {
         type: 'input',
@@ -98,6 +151,7 @@ const promptUser = data => {
             }
         }
         },
+
         {
         type: 'input',
         name: 'managerOfficeNumber',
@@ -111,43 +165,144 @@ const promptUser = data => {
             }
         }
         },
+        
+
+
+        
+        
+
+    ])
+} 
+
+const promptUser = () => {
+    return inquirer.prompt([
+        
+        
+
+
         {
-        type: 'input', ///'list'
+        type: 'list', ///'list'
         name: 'menuOption',
         message: 'Would you like to add an engineer, an intern, or are you finished building your team?',
         choices: ['Add an Engineer', 'Add an Intern', 'Finished Building'],
-        validate: myMenuOption => {
+        //validate: myMenuOption => {
 
 
-            if (myMenuOption === "Add an Engineer" || myMenuOption === 'add an engineer') {
-                return true
+        //    if (myMenuOption === "Add an Engineer" || myMenuOption === 'add an engineer') {
+             //   return true
                
                 
-            } else if (myMenuOption === "Add an Intern" || myMenuOption === "add an intern") {
-                return true
-            } else if (myMenuOption === 'Finished Building' || myMenuOption === 'finished building') {
-                return true
-            } else {
-                console.log('Please input an appropriate answer.')
-                return false
-            }
+          //  } else if (myMenuOption === "Add an Intern" || myMenuOption === "add an intern") {
+              //  return true
+          //  } else if (myMenuOption === 'Finished Building' || myMenuOption === 'finished building') {
+             //   return true
+           // } else {
+              //  console.log('Please input an appropriate answer.')
+             //   return false
+           // }
             
-        }
+        //}
          
 
 
         },
+        {
+        type: 'input',
+        name: 'engineerName',
+        message: 'What is the name of the engineer?',
+        when: (data) => data.menuOption === 'Add an Engineer'
+        },
+        {
+        type: 'input',
+        name: 'engineerID',
+        message: 'What is the ID of the engineer?',
+        when: (data) => data.menuOption === 'Add an Engineer'
+        },
+        {
+        type: 'input',
+        name: 'engineerEmail',
+        message: 'What is the email of the engineer?',
+        when: (data) => data.menuOption === 'Add an Engineer'
+        },
+        {
+        type: 'input',
+        name: 'engineerGithub',
+        message: 'What is the github name of the engineer?',
+        when: (data) => data.menuOption === 'Add an Engineer'
+        },
+        {
+        type: 'input',
+        name: 'internName',
+        message: 'What is the name of the intern?',
+        when: (data) => data.menuOption === 'Add an Intern'
+        },
+        {
+        type: 'input',
+        name: 'internID',
+        message: 'What is the id of the intern?',
+        when: (data) => data.menuOption === 'Add an Intern'
+        },
+        {
+        type: 'input',
+        name: 'internEmail',
+        message: 'What is the email of the intern?',
+        when: (data) => data.menuOption === 'Add an Intern'
+        },
+        {
+        type: 'input',
+        name: 'internSchool',
+        message: 'What is the school of the intern?',
+        when: (data) => data.menuOption === 'Add an Intern'
+        },
+        
 
     ])
-} 
-//console.log(answers))
-promptUser().then(answers => fs.writeFile('./dist/index.html', writeToHTML(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber), err => {
-    if (err) {
-        throw err;
-    }
+}
+
+
+
+
+
+
+
+promptManager().then(managerAnswers => {
+    employeeList.push(new Manager (managerAnswers.managerName, managerAnswers.managerId, managerAnswers.managerEmail, managerAnswers.managerOfficeNumber))
+    listMenu()
+    
 })
 
-)
+const listMenu = () => {
+    promptUser().then(answers => {
+        switch(answers.menuOption) {
+            case "Add an Engineer":
+            employeeList.push(new Engineer(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGithub))
+            listMenu();
+            break;
+    
+            case "Add an Intern":
+            employeeList.push(new Intern (answers.internName, answers.internID, answers.InternEmail, answers.internSchool))
+            listMenu();
+            break;
+    
+            case "Finished Building":
+            fs.writeFile('./dist/index.html', writeToHTML(), err => {
+                if (err) {
+                    throw err
+                }
+            })
+        }
+    })
+}
+
+
+//console.log(answers))
+//promptUser().then(answers => fs.writeFile('./dist/index.html', writeToHTML(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber), err => {
+  //  if (err) {
+    //    throw err;
+    //}
+//})
+
+//)
   //.then(answers => console.log(answers));
 
 
